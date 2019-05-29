7f23a07193990115a9696babdba7c92fce1c4db7
TalifhaniNekhumbe <47749117+TalifhaniNekhumbe@users.noreply.github.com>
2019-05-29 21:13:42 +0200

    Update test_tcase1.php

--- testcase/testcases/test_tcase1.php
+++ testcase/testcases/test_tcase1.php
@@ -5,7 +5,7 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class tcase1Test extends TestCase{
   
  public function test_case(){
-   require_once('/home/travis/build/404-brain-not-found/testcase/tcase1.php');
+   require_once('/home/travis/build/mthanda15/404-brain-not-found/testcase/testcases/tcase1.php');
    $test=case(4,5);
  $this->assertEquals(5,$test, "correct!"); 
  }
