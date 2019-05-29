71b07a49a7a35c1202f33233b032ee98aab57943
TalifhaniNekhumbe <47749117+TalifhaniNekhumbe@users.noreply.github.com>
2019-05-29 21:31:32 +0200

    Update test_tcase1.php

--- testcase/testcases/test_tcase1.php
+++ testcase/testcases/test_tcase1.php
@@ -4,9 +4,9 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 class tcase1Test extends TestCase{
   
- public function test_case(){
+ public function test_nazo(){
    require_once('/home/travis/build/mthanda15/404-brain-not-found/testcase/tcase1.php');
-   $test=case(4,5);
+   $test=nazo(4,5);
  $this->assertEquals(5,$test, "correct!"); 
  }
  
