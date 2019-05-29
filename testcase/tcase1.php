8253a8d442fd7c8ada6312cb66f797654e96971b
TalifhaniNekhumbe <47749117+TalifhaniNekhumbe@users.noreply.github.com>
2019-05-29 21:51:18 +0200

    Update tcase1.php

--- testcase/tcase1.php
+++ testcase/tcase1.php
@@ -3,9 +3,9 @@ function nazo($p,$z){
       if($p<$z){
         return $z;
       }
-      else if($p>$z){
-            return $p;
-      }
+//       else if($p>$z){
+//             return $p;
+//       }
       
             else if($p==$z){
             return $p+$z;
